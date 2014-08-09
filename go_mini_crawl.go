6eb11f9dd07c5831b71bc5e218a3394346115f7a
Mauro Risonho de Paula Assumpcao <mauro.risonho@gmail.com>
2014-08-09 17:16:23 -0300

    Create go_mini_crawl.go

---
+++ go_mini_crawl.go
@@ -0,0 +1,122 @@
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following disclaimer
+ *   in the documentation and/or other materials provided with the
+ *   distribution.
+ * * Neither the name of the  nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Mauro Risonho de Paula Assumpcao - firebitsbr - mauro.risonho@gmail.com
+ * crawler/spider
+ *  
+ */
+
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"github.com/jackdanger/collectlinks"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: crawl http://example.com/path/file.html\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	fmt.Println("Mauro Risonho de Paula Assumpcao - @firebisbr")
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		usage()
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
+
+	queue := make(chan string)
+	filteredQueue := make(chan string)
+
+	go func() { queue <- args[0] }()
+	go filterQueue(queue, filteredQueue)
+
+	// pull from the filtered queue, add to the unfiltered queue
+	for uri := range filteredQueue {
+		enqueue(uri, queue)
+	}
+}
+
+func filterQueue(in chan string, out chan string) {
+	var seen = make(map[string]bool)
+	for val := range in {
+		if !seen[val] {
+			seen[val] = true
+			out <- val
+		}
+	}
+}
+
+func enqueue(uri string, queue chan string) {
+	fmt.Println("fetching", uri)
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client := http.Client{Transport: transport}
+	resp, err := client.Get(uri)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	links := collectlinks.All(resp.Body)
+
+	for _, link := range links {
+		absolute := fixUrl(link, uri)
+		if uri != "" {
+			go func() { queue <- absolute }()
+		}
+	}
+}
+
+func fixUrl(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
+}
